98c34caff868b9afa0c137fb07f876109df89614
Oluwaseyi (Ekpere) Oyewunmi <thoth2357@gmail.com>
2025-05-03 09:53:54 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ['plankton-app-gd57a.ondigitalocean.app'] //added this    
   },
   plugins: [
     react(),
